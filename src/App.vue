3acdccd63dfdd1dbf4644dbecb41c752c53c4ca6
YassineZhang <57878778@qq.com>
2024-11-22 21:48:02 +0800

    feat: Use shadcnui frame and tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,5 @@
 <script setup lang="ts"></script>
 
 <template>
-  <ant-config-provider
-    :theme="{
-      token: {
-        fontFamily:
-          'ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji',
-      },
-    }"
-  >
-    <RouterView class="gt-standard" />
-  </ant-config-provider>
+  <RouterView class="gt-standard" />
 </template>
